a7ee1f79a70ca0e928fdf0832c426299e387a31b
alkaid <alkaid3529@163.com>
2023-12-04 12:48:36 +0800

    copy upload

---
+++ sample/my_app/hello_world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "ohos_init.h"
+
+void Hello_World(void)
+{
+    printf("Hello World!\r\n");
+    printf("Hello World!\r\n");
+    printf("Hello World!\r\n");
+    printf("Hello World!\r\n");
+    printf("Hello World!\r\n");
+    printf("Hello World!\r\n");
+    printf("***************************\r\n");
+}
+APP_FEATURE_INIT(Hello_World);
